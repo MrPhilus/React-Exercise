c98bed6dd1883cfb2dd5776c6c18a6b5af7b4c0b
Theophilus Oyeyemi <105336834+MrPhilus@users.noreply.github.com>
2023-07-22 16:50:37 +0100

    Functional changes made

--- react-exercise/src/App.jsx
+++ react-exercise/src/App.jsx
@@ -2,24 +2,21 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [product, setProduct] = useState([]);
-  const products = [{}];
+  const [product, setProduct] = useState({});
+  const [inStock, setInStock] = useState(1);
 
   function nextItem() {
-    setBrand((previous) => previous + 1);
+    setInStock((prevStock) => prevStock + 1);
+    console.log(prevStock);
   }
 
   function prevItem() {
-    setBrand((previous) => previous - 1);
+    setInStock((prevStock) => prevStock - 1);
+    console.log(prevStock);
   }
 
   const productList = () => {
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch("https://dummyjson.com/products/1", requestOptions)
+    fetch(`https://dummyjson.com/products/${inStock}`)
       .then((response) => response.json())
       .then((result) => {
         setProduct(result);
@@ -30,22 +27,32 @@ function App() {
 
   useEffect(() => {
     productList();
-  }, []);
+  }, [inStock]);
 
   return (
     <div className="App">
       <section className="card">
         <button onClick={prevItem}>Prev</button>
         <div className="cardBox">
-          <h3>{product.title}</h3>
-          <img width={"100px"} src={product.thumbnail} alt="" />
-          <p>{product.description}</p>
-          <span>${product.price}</span>
+          <h3>{product?.title}</h3>
+          <img width={"200px"} src={product?.thumbnail} alt="" />
+          <p style={{ fontSize: "12px", padding: "10px" }}>
+            {product?.description}
+          </p>
+          <p style={{ padding: "10px" }}>${product?.price}</p>
         </div>
         <button onClick={nextItem}>Next</button>
       </section>
+      <input
+        type="number"
+        onKeydown={(e) => {
+          setProduct(Number(e.target.value));
+        }}
+      />
     </div>
   );
 }
 
 export default App;
+
+//add a function to change product on input
